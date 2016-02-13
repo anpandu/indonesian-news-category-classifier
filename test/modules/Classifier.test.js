88af1fe2493c18594cfe463ec38769bfd50f5f7e
anpandu <anpandumail@gmail.com>
2016-02-13 19:26:18 +0700

    Classifier: classify

--- test/modules/Classifier.test.js
+++ test/modules/Classifier.test.js
@@ -12,4 +12,10 @@ describe('Classifier', function () {
     assert(_.isObject(Classifier.model), 'empty model')
     assert(_.isEqual(Classifier.model, answer))
   })
+
+  it('classify', function () {
+    var scores = [['edukasi',0],['health',0],['otomotif',0.03595302533370599],['travel',0.3185146904481461],['lifestyle',0.024128275486337593],['techno',0.10048398717407839],['internasional',0.10504621790818398],['entertainment',0.08531553564656265],['ibukota',0.04273118574437586],['bisnis',0.16635922529394154],['sport',0.04591184898444333],['nasional',0.07555600798022455]]
+    var result = Classifier.classify(scores)
+    assert(_.isEqual(result, 'bisnis'))
+  })
 })
