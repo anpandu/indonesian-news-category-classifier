9fc95d9fc4b91d7a6264cda59495253cb504ee7e
anpandu <anpandumail@gmail.com>
2016-02-13 17:46:29 +0700

    Preprocess: process

--- test/modules/Preprocess.test.js
+++ test/modules/Preprocess.test.js
@@ -29,4 +29,11 @@ describe('Preprocess', function () {
   	var answer = [['edukasi',0],['health',0.013179993339527183],['otomotif',0.006944538961005934],['travel',0.00468372455468156],['lifestyle',0.006553536617307781],['techno',0.0059976401687693226],['internasional',0.013970809906758731],['entertainment',0.026342190273355503],['ibukota',0.00823981083667967],['bisnis',0.011703977125550982],['sport',0.8923837793151527],['nasional',0.009999998901210611]]
     assert(_.isEqual(answer, scores))
   })
+
+  it('process', function () {
+  	var text = 'Kapal feri Sea Prince yang mengangkut sebanyak 100 orang dari Singapura menuju Indonesia tenggelam di Batam'
+  	var answer = {text:'Kapal feri Sea Prince yang mengangkut sebanyak 100 orang dari Singapura menuju Indonesia tenggelam di Batam',scores:[['edukasi',0],['health',0],['otomotif',0.03595302533370599],['travel',0.3185146904481461],['lifestyle',0.024128275486337593],['techno',0.10048398717407839],['internasional',0.10504621790818398],['entertainment',0.08531553564656265],['ibukota',0.04273118574437586],['bisnis',0.16635922529394154],['sport',0.04591184898444333],['nasional',0.07555600798022455]]}
+  	var result = Preprocess.process(text)
+    assert(_.isEqual(answer, result))
+  })
 })
