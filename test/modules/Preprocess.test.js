e693ce010fbd80e52a4b1d054a2c7844475bd9bf
anpandu <anpandumail@gmail.com>
2016-02-13 17:33:01 +0700

    Preprocess: getScores

--- test/modules/Preprocess.test.js
+++ test/modules/Preprocess.test.js
@@ -21,4 +21,12 @@ describe('Preprocess', function () {
   	var tokens = Preprocess.getToken(text)
     assert(_.isEqual(answer, tokens))
   })
+
+  it('getScores', function () {
+  	var text = 'Southampton - Liverpool menang besar kala bertandang ke Southampton di babak kelima Piala Liga Inggris. Tertinggal lebih dahulu, Liverpool bikin 6 gol balasan di mana Divock Origi mencetak hat-trick dan Daniel Sturridge dua gol.'
+  	var tokens = Preprocess.getToken(text)
+  	var scores = Preprocess.getScores(tokens)
+  	var answer = [['edukasi',0],['health',0.013179993339527183],['otomotif',0.006944538961005934],['travel',0.00468372455468156],['lifestyle',0.006553536617307781],['techno',0.0059976401687693226],['internasional',0.013970809906758731],['entertainment',0.026342190273355503],['ibukota',0.00823981083667967],['bisnis',0.011703977125550982],['sport',0.8923837793151527],['nasional',0.009999998901210611]]
+    assert(_.isEqual(answer, scores))
+  })
 })
